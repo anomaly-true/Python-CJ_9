54246e55ddfc944e5552ace4434dfba9b771e275
CaedenPH <caedenperelliharris@gmail.com>
2022-07-22 16:10:17 +0100

    refactor: Show main window with qasync

---
+++ src/client/main.py
@@ -0,0 +1,27 @@
+import asyncio
+import sys
+
+import qasync
+from aiohttp import ClientSession, ClientTimeout
+from views import login
+
+
+async def main():
+    """Starts the login window."""
+    loop = asyncio.get_event_loop()
+
+    session = ClientSession(
+        loop=asyncio.get_event_loop(),
+        timeout=ClientTimeout(total=10),
+    )
+
+    main_window = login.Window(loop, session)
+    main_window.show()
+
+    await asyncio.Future()
+
+if __name__ == "__main__":
+    try:
+        qasync.run(main())
+    except asyncio.CancelledError:
+        sys.exit(0)
