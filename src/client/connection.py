f06b14474da96b1f4c5313a79d2723a255d4d28d
CaedenPH <caedenperelliharris@gmail.com>
2022-07-22 16:01:16 +0100

    refactor: Move connection to seperate file

---
+++ src/client/connection.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+import asyncio
+from typing import Any, Dict
+
+from aiohttp import ClientSession, ClientWebSocketResponse, WSMsgType
+
+
+class WebsocketHandler:
+    """
+    Represents a websocket connection.
+
+    Attributes
+    ----------
+    MESSAGE
+        A message has been sent from a client.
+    """
+
+    MESSAGE = 0
+
+    def __init__(
+        self,
+        *,
+        websocket: ClientWebSocketResponse,
+        session: ClientSession,
+        loop: asyncio.AbstractEventLoop,
+    ) -> None:
+        self.socket = websocket
+        self.session = session
+        self.loop = loop
+
+    @classmethod
+    async def from_client(
+        cls, session: ClientSession, loop: asyncio.AbstractEventLoop
+    ) -> WebsocketHandler:
+        """Handles the websocket connection.
+
+        :param session: The session used to make the websocket connection.
+        :param loop: The event loop used to make the websocket connection.
+        """
+        websocket = await session.ws_connect("http://127.0.0.1:8080/ws")
+        self = cls(websocket=websocket, session=session, loop=loop)
+        return self
+
+    async def parse(self, data: Dict[Any, Any]):
+        """Parses messages from the websocket connection.
+
+        :param data: The data received in dict format.
+        """
+        print(data)
+        await self.socket.send_json(data)
+
+    async def listen(self):
+        """Listens to incoming websocket messages."""
+        async for message in self.socket:
+            if message.type == WSMsgType.TEXT:
+                await self.parse(message.data)
