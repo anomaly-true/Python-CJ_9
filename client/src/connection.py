98cee5b13da23edadb9ca1e9bde2b24e1468342c
CaedenPH <caedenperelliharris@gmail.com>
2022-07-22 21:42:00 +0100

    feat: WebsocketConnection dataclass and pass token into ws handshake

--- client/src/connection.py
+++ client/src/connection.py
@@ -1,9 +1,23 @@
 from __future__ import annotations
 
-import asyncio
+from dataclasses import dataclass
 from typing import Any, Dict
 
 from aiohttp import ClientSession, ClientWebSocketResponse, WSMsgType
+
+
+@dataclass
+class WebsocketConnection:
+    """Dataclass to hold information about the WebSocket connection."""
+
+    username: str
+    session: ClientSession
+    websocket: ClientWebSocketResponse
+
+    def __init__(self, username: str, session: ClientSession, websocket: WebsocketHandler) -> None:
+        self.username = username
+        self.session = session
+        self.websocket = websocket
 
 
 class WebsocketHandler:
@@ -22,24 +36,22 @@ class WebsocketHandler:
         self,
         *,
         websocket: ClientWebSocketResponse,
-        session: ClientSession,
-        loop: asyncio.AbstractEventLoop,
     ) -> None:
         self.socket = websocket
-        self.session = session
-        self.loop = loop
 
     @classmethod
-    async def from_client(
-        cls, session: ClientSession, loop: asyncio.AbstractEventLoop
+    async def from_user(
+        cls,
+        session: ClientSession,
+        token: str
     ) -> WebsocketHandler:
         """Handles the websocket connection.
 
         :param session: The session used to make the websocket connection.
         :param loop: The event loop used to make the websocket connection.
         """
-        websocket = await session.ws_connect("http://127.0.0.1:8080/ws")
-        self = cls(websocket=websocket, session=session, loop=loop)
+        websocket = await session.ws_connect(f"http://127.0.0.1:8080/ws/{token}")
+        self = cls(websocket=websocket)
         return self
 
     async def parse(self, data: Dict[Any, Any]):
