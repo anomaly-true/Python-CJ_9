2f2738308332499f58f1fe3aaded0450266dccca
CaedenPH <caedenperelliharris@gmail.com>
2022-07-22 21:58:19 +0100

    feat: Append message to the home window in parse

--- client/src/connection.py
+++ client/src/connection.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Any, Dict
+from typing import TYPE_CHECKING, Any, Dict
 
 from aiohttp import ClientSession, ClientWebSocketResponse, WSMsgType
+
+if TYPE_CHECKING:
+    from views import home
 
 
 @dataclass
@@ -12,12 +15,21 @@ class WebsocketConnection:
 
     username: str
     session: ClientSession
-    websocket: ClientWebSocketResponse
+    websocket: WebsocketHandler
 
     def __init__(self, username: str, session: ClientSession, websocket: WebsocketHandler) -> None:
         self.username = username
         self.session = session
         self.websocket = websocket
+
+    async def send(self, message: Dict[Any, Any]) -> None:
+        """Sends a message though the WebSocket connection.
+
+        Shorthand for calling `self.connection.websocket.socket.send_json`
+
+        :param message: The message to send.
+        """
+        await self.websocket.socket.send_json(message)
 
 
 class WebsocketHandler:
@@ -54,16 +66,22 @@ class WebsocketHandler:
         self = cls(websocket=websocket)
         return self
 
-    async def parse(self, data: Dict[Any, Any]):
+    async def parse(self, data: Dict[Any, Any], home_window: home.Window):
         """Parses messages from the websocket connection.
 
         :param data: The data received in dict format.
+        :param home_window: The home window.
         """
-        print(data)
-        await self.socket.send_json(data)
+        op = data.get("op")
 
-    async def listen(self):
-        """Listens to incoming websocket messages."""
+        if op == self.MESSAGE:
+            home_window.append_message(**data["data"])
+
+    async def listen(self, home_window: home.Window):
+        """Listens to incoming websocket messages.
+
+        :param home_window: The home window.
+        """
         async for message in self.socket:
             if message.type == WSMsgType.TEXT:
-                await self.parse(message.data)
+                await self.parse(message.data, home_window)
