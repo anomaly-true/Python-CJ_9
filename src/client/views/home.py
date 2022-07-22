d76d2c2d135ba24081ad79385ac6a7d45fc22fb7
CaedenPH <caedenperelliharris@gmail.com>
2022-07-22 16:08:46 +0100

    feat: Template home window

---
+++ src/client/views/home.py
@@ -0,0 +1,9 @@
+from PyQt5 import QtWidgets
+
+
+class Window(QtWidgets.QMainWindow):
+    """Represents the home window.
+
+    Used to connect the websocket connection and the
+    user input.
+    """
