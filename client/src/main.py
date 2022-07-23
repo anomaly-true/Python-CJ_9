0bf010d55e6cd468e51e78c22d120c3662ec2d57
CaedenPH <caedenperelliharris@gmail.com>
2022-07-23 17:23:17 +0100

    refactor: Don't pass loop into login window

--- client/src/main.py
+++ client/src/main.py
@@ -8,14 +8,12 @@ from views import login
 
 async def main():
     """Starts the login window."""
-    loop = asyncio.get_event_loop()
-
     session = ClientSession(
         loop=asyncio.get_event_loop(),
         timeout=ClientTimeout(total=10),
     )
 
-    login_window = login.Window(loop, session)
+    login_window = login.Window(session)
     login_window.show()
 
     await asyncio.Future()
