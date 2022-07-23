c43d5bb6a048d0d7ab4ca20b2cdd3b43cf5ec760
CaedenPH <caedenperelliharris@gmail.com>
2022-07-23 17:16:57 +0100

    feat: Constants to hold all stylesheet themes and feature messages

---
+++ client/src/constants.py
@@ -0,0 +1,22 @@
+FEATURE_MESSAGES = {
+    "centralwidget": (
+        "When you move your mouse alot the theme changes..."
+        "Talk about getting excersise while coding."
+    ),
+    "loginform": (
+        "The password is useless without the email..."
+        "You can guess a password but the email is longer so harder to guess."
+    ),
+    "toggle": "What's a graphical user interface for if not to excite the user?",
+}
+ALL_THEMES = [
+    "amber",
+    "blue",
+    "cyan",
+    "lightgreen",
+    "pink",
+    "purple",
+    "red",
+    "teal",
+    "yellow",
+]
